9f8ca9a5372a86b1363df363b753be0c624bd62a
Bhavya Soriya <bhavyasoriya@gmail.com>
2021-09-28 14:11:08 +0530

    Added More Routes to Linkage

--- server/routes/linkage.js
+++ server/routes/linkage.js
@@ -17,4 +17,57 @@ linkageRouter
   .put(universalCtrl.requestNotAccepted)
   .delete(universalCtrl.requestNotAccepted);
 
+linkageRouter
+  .route('/:l_id')
+  .get(universalCtrl.validateObjectId, linkageCtrl.getLinkage)
+  .put(
+    auth.verifyUser,
+    auth.verifyOwner,
+    universalCtrl.validateObjectId,
+    linkageCtrl.editLinkage
+  )
+  .delete(
+    auth.verifyUser,
+    auth.verifyOwner,
+    universalCtrl.validateObjectId,
+    linkageCtrl.deleteLinkage
+  )
+  .post(universalCtrl.requestNotAccepted);
+
+linkageRouter
+  .route('/:l_id/subLinkages')
+  .get(universalCtrl.validateObjectId, linkageCtrl.getSubLinkages)
+  .put(universalCtrl.requestNotAccepted)
+  .delete(universalCtrl.requestNotAccepted)
+  .post(universalCtrl.requestNotAccepted);
+
+linkageRouter
+  .route('/:l_id/share')
+  .get(
+    auth.verifyUser,
+    auth.verifyOwner,
+    universalCtrl.validateObjectId,
+    linkageCtrl.getSharingDetails
+  )
+  .put(
+    auth.verifyUser,
+    auth.verifyOwner,
+    universalCtrl.validateObjectId,
+    linkageCtrl.editSharingDetails
+  )
+  .post(universalCtrl.requestNotAccepted)
+  .delete(universalCtrl.requestNotAccepted);
+
+linkageRouter
+  .route('/:l_id/move')
+  .post(
+    auth.verifyUser,
+    auth.verifyOwner,
+    universalCtrl.validateObjectId,
+    linkageCtrl.moveLinkage
+  )
+  .get(universalCtrl.requestNotAccepted)
+  .put(universalCtrl.requestNotAccepted)
+  .delete(universalCtrl.requestNotAccepted);
+
 module.exports = linkageRouter;
