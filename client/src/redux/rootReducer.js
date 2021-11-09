74c1bc0ea61079af203e41dfeed55f16b2a001a0
Yash Brahmkshatriya <yash311000@gmail.com>
2021-11-09 16:39:32 +0530

    Fix root reducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -5,16 +5,12 @@ import {
   LinkageSharedDetails,
 } from './linkage/LinkageReducer';
 import { UserReducer } from './user/UserReducer';
-import { CiphageReducer } from './ciphage/CiphageReducer';
-import { urlReducer } from './urlShort/urlReducer';
 
 const rootReducer = combineReducers({
   linkage: LinkageReducer,
   path: LinkagePathReducer,
   sharedDetails: LinkageSharedDetails,
   user: UserReducer,
-  ciphage: CiphageReducer,
-  urls: urlReducer,
 });
 
 export default rootReducer;
