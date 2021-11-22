0fc879a42c8b8869d5b635dee8d5abb49ef5ea57
Yash Brahmkshatriya <yash311000@gmail.com>
2021-11-22 16:08:34 +0530

    Add backend api support

--- client/src/shared/envVars.js
+++ client/src/shared/envVars.js
@@ -1,2 +1,5 @@
 export const ENV = process.env.REACT_APP_ENV;
-export const apiUrl = ENV === 'DEVELOPMENT' ? 'http://localhost:8000' : '';
+export const apiUrl =
+  ENV === 'DEVELOPMENT'
+    ? 'http://localhost:8000'
+    : process.env.REACT_APP_PROD_API_URL;
